6b6e03116d7c934a5a0026f47f8d846462b30235
momodemidoun1 <momodemidoun1@gmail.com>
2019-10-25 20:32:14 +0100

    Javascript notes for professional

---
+++ script.js
@@ -0,0 +1,37 @@
+let a = function() {
+  return true;
+};
+
+let b = {
+  name: "Mohamed",
+  lname: "Rached"
+};
+
+var greet = "Hello",
+  who = "World",
+  me = "Mohamed";
+
+// console.log("%s, %s??%s", greet, who, me);
+console.log({
+  Email: "",
+  Groups: {},
+  Id: 33,
+  IsHiddenInUI: false,
+  IsSiteAdmin: false,
+  LoginName: "i:0#.w|virtualdomain\\user2",
+  PrincipalType: 1,
+  Title: "user2"
+});
+
+document.getElementById("paragraph").innerHTML = "hhhhh";
+let element = document.createElement("p");
+element.textContent = "Hello, world";
+document.body.appendChild(element);
+
+jhon = {
+  nom: "mohamed",
+  lname: "Rached",
+  a: [true, undefined, 5]
+};
+x = 1;
+console.log(typeof NaN);
